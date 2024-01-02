3644f2c850f740d6eddc27eca050144593da1391
heewoong-ahn <dksgmldnd@korea.ac.kr>
2024-01-03 03:06:27 +0900

    splash gif add

--- app/src/main/java/com/heewoong/threebasicscreens/splashActivity.kt
+++ app/src/main/java/com/heewoong/threebasicscreens/splashActivity.kt
@@ -8,29 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class splashActivity : AppCompatActivity() {
-    private lateinit var madCamp: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        Glide.with(this).load(R.raw.snap).into(snap)
 
 
-        madCamp = findViewById(R.id.madCamp)
-         fun moveToFront() {
 
-             val scaleXAnimator: ValueAnimator = ObjectAnimator.ofFloat(madCamp, "scaleX", 1f, 4f)
-             scaleXAnimator.duration = 3000
 
-             val scaleYAnimator: ValueAnimator = ObjectAnimator.ofFloat(madCamp, "scaleY", 1f, 4f)
-             scaleYAnimator.duration = 3000
-
-             val animatorSet = AnimatorSet()
-             animatorSet.playTogether( scaleXAnimator, scaleYAnimator)
-             animatorSet.start()
-        }
-        moveToFront()
 
 
         Handler().postDelayed({
